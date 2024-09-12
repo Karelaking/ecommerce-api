616e1e3af81b2a357da6add51c71d97dcd699e57
mradul kumar katiyar <karelaking277@gmai.com>
2024-09-12 21:55:59 +0530

    feat: Add middleware, route handling, URL_PREFIX, and db test

--- src/app.js
+++ src/app.js
@@ -1,12 +1,14 @@
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
-import { JSON_LIMIT, STATIC_ASSETS_URL } from "./constants.js";
+import { JSON_LIMIT, STATIC_ASSETS_URL, URL_PREFIX } from "./constants.js";
 
 const CORS_ORIGIN = process.env.CORS_ORIGIN;
 
-// Initialize the express application with cors, json, urlencoded and cookieparsers middleware.
+// Initialize the express application
 const app = express();
+
+// apply cors middleware
 app.use(
   cors({
     origin: CORS_ORIGIN,
@@ -14,12 +16,14 @@ app.use(
   })
 );
 
+// apply json middleware
 app.use(
   express.json({
     limit: JSON_LIMIT,
   })
 );
 
+// apply urlencoded middleware
 app.use(
   express.urlencoded({
     extended: true,
@@ -27,8 +31,24 @@ app.use(
   })
 );
 
+// apply cookie-parser middleware
 app.use(cookieParser());
 
+// apply static assets middleware
 app.use(express.static(STATIC_ASSETS_URL));
+
+// import routes
+import userRouter from "./routes/user.routes.js";
+import authRouter from "./routes/auth.routes.js";
+import productsRouter from "./routes/products.routes.js";
+import ordersRouter from "./routes/orders.routes.js";
+import cartRouter from "./routes/cart.routes.js";
+
+// apply routes
+app.use(`${URL_PREFIX}/auth`, authRouter);
+app.use(`${URL_PREFIX}/users`, userRouter);
+app.use(`${URL_PREFIX}/products`, productsRouter);
+app.use(`${URL_PREFIX}/orders`, ordersRouter);
+app.use(`${URL_PREFIX}/cart`, cartRouter);
 
 export default app;
--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,6 @@
 const DB_NAME = "eCommerce";
 const JSON_LIMIT = "16kb";
 const STATIC_ASSETS_URL = "public";
+const URL_PREFIX = "/api/v1";
 
-export { DB_NAME, JSON_LIMIT, STATIC_ASSETS_URL };
+export { DB_NAME, JSON_LIMIT, STATIC_ASSETS_URL, URL_PREFIX };
